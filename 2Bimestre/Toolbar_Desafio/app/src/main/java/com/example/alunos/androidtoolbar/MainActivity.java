b262002421bc33b3d81f3a297daae6ca75d350ba
ThalitaMaria <thalitamaria62@gmail.com>
2018-06-04 09:10:26 -0300

    Organizando

--- 2Bimestre/Toolbar_Desafio/app/src/main/java/com/example/alunos/androidtoolbar/MainActivity.java
+++ 2Bimestre/Toolbar_Desafio/app/src/main/java/com/example/alunos/androidtoolbar/MainActivity.java
@@ -55,8 +55,6 @@ public class MainActivity extends AppCompatActivity {
                             f = new AddItemsFragment();
                         }else if(itemId == R.id.action_view_items){
                             f = new ViewItensFragment();
-                        }else if(itemId == R.id.settings){
-                            f = new SettingsFragment();
                         }
                         if(f != null){
                             FragmentTransaction transaction =
